5a1cadfb7f973e7c75d2c4230018ebc5943a9ba0
Nik <41297691+CyberEssence@users.noreply.github.com>
2022-05-31 09:23:31 +0200

    Update apps.py

--- project/core/apps.py
+++ project/core/apps.py
@@ -7,6 +7,8 @@
 from django.apps import AppConfig
 
 
+# иницилизация приложения с конфигом движка сайта
+
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
