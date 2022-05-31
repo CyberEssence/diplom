a79eea97c508db8c3f86628d2ad77043dfcad241
Nik <41297691+CyberEssence@users.noreply.github.com>
2022-05-31 09:26:50 +0200

    Update views.py

--- project/core/views.py
+++ project/core/views.py
@@ -9,16 +9,19 @@ from django.http.response import HttpResponse
 from django.http import HttpResponse
 from proj.models import Post
 
-# Create your views here.
+# создание views приложения core
+
+# рендер главной страницы
 def frontpage(request):
     posts = Post.objects.filter(status=Post.ACTIVE)
     return render(request, 'core/frontpage.html', {'posts': posts})
 
-
+# рендер страницы с about
 def about(request):
     return render(request, 'core/about.html')
 
 
+# файл robots.txt с настройками
 def robots_txt(request):
     text = [
         "User-Agent: *",
